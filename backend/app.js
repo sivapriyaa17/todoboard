4c3ed35b8e29869eee5c2d30dee10f3ed907710a
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-08 21:11:49 +0530

    add user

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,11 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
