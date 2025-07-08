6efb1d1d9e3c94f569f51b0bab67058cd3e70532
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-08 21:36:56 +0530

    add backend url

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
+
 const http = require('http');
 const { Server } = require('socket.io');
-const app = require('./app');
+const app = require('./app'); 
 const socketSetup = require('./sockets');
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://todoboard-pgz1.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
 });
 
+
 app.set('io', io);
+
+
 socketSetup(io);
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
